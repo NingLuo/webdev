ef98b012e6e30a1c896a301ecf9acf186ca307d5
Ning Luo <luo.n@husky.neu.edu>
2016-02-18 16:37:12 -0500

    Tested controller in experiment with movies

--- public/experiments/AngularJS/app.js
+++ public/experiments/AngularJS/app.js
@@ -8,9 +8,50 @@
     function MoviesController($scope){
         console.log("Hello from MoveisController");
         movies = [
-            {name: 'Harry Potter', director: 'Rolin', Date: '2000'},
-            {name: 'Star War', director: 'JJ', Date: '1989'},
+            {name: 'Harry Potter', director: 'Rolin', year: '2000'},
+            {name: 'Star War', director: 'JJ', year: '1989'},
         ];
         $scope.movies = movies;
+
+
+        $scope.addMovie = addMovie;
+        $scope.removeMovie = removeMovie;
+        $scope.selectMovie = selectMovie;
+        $scope.updateMovie = updateMovie;
+
+
+        function addMovie(movie) {
+            console.log(movie);
+            var newMovie = {
+                name: movie.name,
+                director: movie.director,
+                year: movie.year
+            };
+            $scope.movie = {};
+            $scope.movies.push(newMovie);
+        }
+
+        function removeMovie(movie) {
+            console.log("Remove movie: " + movie.name);
+            var index = $scope.movies.indexOf(movie);
+            $scope.movies.splice(index, 1);
+        }
+
+        function selectMovie(movie) {
+            $scope.selectedMovieIndex = $scope.movies.indexOf(movie);
+            $scope.movie = {
+                name: movie.name,
+                director: movie.director,
+                year: movie.year
+            };
+        }
+
+        function updateMovie(movie) {
+            $scope.movies[$scope.selectedMovieIndex] = {
+                name: movie.name,
+                director: movie.director,
+                year: movie.year
+            };
+        }
     };
 })();
