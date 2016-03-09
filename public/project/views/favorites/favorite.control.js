1dc770669341acd3ecc0c9b308c6a9673bfd813a
Ning Luo <luo.n@husky.neu.edu>
2016-03-09 15:44:34 -0500

    implemented unfavorite doctor

--- public/project/views/favorites/favorite.control.js
+++ public/project/views/favorites/favorite.control.js
@@ -8,17 +8,36 @@
     function FavoriteCtrl ($rootScope, DoctorSearchService) {
         var vm = this;
         vm.favoriteDoctors = [];
+        vm.unfavorite = unfavorite;
         var currentUser = $rootScope.currentUser;
         var length = currentUser.favorites.length;
 
-        //由于http请求有失败几率,所以这块要想办法用循环来保证成功率
-        for(var i = 0; i < length; i++) {
-            var uid = currentUser.favorites[i];
-            DoctorSearchService.findDoctorByUid(uid)
-                .then(function (response) {
-                    console.log(response.data);
-                    vm.favoriteDoctors.push(response.data.data);
-                });
+        function init() {
+            //由于http请求有失败几率,所以这块要想办法用循环来保证成功率
+            for(var i = 0; i < length; i++) {
+                var uid = currentUser.favorites[i];
+                DoctorSearchService.findDoctorByUid(uid)
+                    .then(function (response) {
+                        console.log(response.data);
+                        vm.favoriteDoctors.push(response.data.data);
+                    });
+            }
+        }
+        init();
+
+        function unfavorite(doctor) {
+            //删除掉用户的favorites里的doctor id
+            for(var i=0; i<length; i++) {
+                if(currentUser.favorites[i] == doctor.uid) {
+                    currentUser.favorites.splice(i, 1);
+                }
+            }
+            //删除掉从http成功取回的docotr list里的doctor对象, 为了使其从view里消失
+            for(var i=0; i<vm.favoriteDoctors.length; i++) {
+                if(vm.favoriteDoctors[i].uid == doctor.uid) {
+                    vm.favoriteDoctors.splice(i, 1);
+                }
+            }
         }
     }
 })();
