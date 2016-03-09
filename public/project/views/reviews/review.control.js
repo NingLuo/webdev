74dcf6f6d7dba98a62cd62c728128b6dc49894c9
Ning Luo <luo.n@husky.neu.edu>
2016-03-09 15:18:38 -0500

    implemented deleteReview

--- public/project/views/reviews/review.control.js
+++ public/project/views/reviews/review.control.js
@@ -9,6 +9,7 @@
         var vm = this;
         vm.reviews = null;
         vm.edit = edit;
+        vm.deleteReview= deleteReview;
 
         function init() {
             vm.reviews =  UserService.findReviewsByUserId($rootScope.currentUser.u_id);
@@ -18,6 +19,10 @@
         function edit(review) {
             console.log(review.id);
             $location.url('editReview/'+ review.id);
+        }
+
+        function deleteReview(index) {
+            vm.reviews.splice(index, 1);
         }
     }
 })();
