3e75079e9ba973fc56ca2b47b125be61f8abbc81
Ning Luo <luo.n@husky.neu.edu>
2016-03-16 23:13:17 -0400

    implement CRUD in user.model

--- public/assignment/server/models/user.model.js
+++ public/assignment/server/models/user.model.js
@@ -4,7 +4,11 @@ module.exports = function() {
     var api = {
         findUserByUsername: findUserByUsername,
         findUserByCredentials: findUserByCredentials,
-        createUser: createUser
+        createUser: createUser,
+        findAllUsers: findAllUsers,
+        findUserById: findUserById,
+        updateUser: updateUser,
+        deleteUser: deleteUser
     };
     return api;
 
@@ -30,5 +34,39 @@ module.exports = function() {
         user._id = "ID_" + (new Date).getTime();
         mock.push(user);
         return user;
+    }
+
+    function findAllUsers() {
+        return moke;
+    }
+
+    function findUserById(userId) {
+        for(var i in mock) {
+            if(mock[i]._id == userId){
+                return mock[i];
+            }
+        }
+        return null;
+    }
+
+    function updateUser(userId, new_user) {
+        var user = findUserById(userId);
+        if(user) {
+            user = {
+                "_id": new_user._id,
+                "firstName": new_user.firstName,
+                "lastName": new_user.lastName,
+                "username": new_user.username,
+                "password": new_user.password
+            }
+        }
+    }
+
+    function deleteUser(userId) {
+        for(var i in mock) {
+            if(mock[i]._id == userId) {
+                mock.splice(i, 1);
+            }
+        }
     }
 }
