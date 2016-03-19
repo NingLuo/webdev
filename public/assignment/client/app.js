2cd1fe206e3463a24dd75f3feaa58acfefdf49f4
Ning Luo <luo.n@husky.neu.edu>
2016-03-18 22:12:38 -0400

    switch lnSortable to ui Sortable of angular ui

--- public/assignment/client/app.js
+++ public/assignment/client/app.js
@@ -1,5 +1,5 @@
 (function () {
     "use strict";
 
-    angular.module("FormBuilderApp", ["ngRoute", "lnSortable", 'ui.bootstrap']);
+    angular.module("FormBuilderApp", ["ngRoute", "lnSortable", "ui.sortable",'ui.bootstrap']);
 })();
