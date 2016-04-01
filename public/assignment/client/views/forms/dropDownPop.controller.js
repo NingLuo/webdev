615a1865215f5098c903e39819fdb6b0ff62df2d
Ning Luo <luo.n@husky.neu.edu>
2016-04-01 18:02:31 -0400

    implement editing options in dropdown Pop, save it in mongoDB

--- public/assignment/client/views/forms/dropDownPop.controller.js
+++ public/assignment/client/views/forms/dropDownPop.controller.js
@@ -8,17 +8,36 @@
 
         vm.field = field;
         vm.update = update;
+        vm.close = close;
+        vm.optStr = "";
 
-        vm.close = function close() {
-            $uibModalInstance.dismiss('cancel');
-        };
+        //Convert option objects to a string
+        for(var i in field.options)
+        {
+            vm.optStr = vm.optStr.concat(field.options[i].label);
+            vm.optStr = vm.optStr.concat(":");
+            vm.optStr = vm.optStr.concat(field.options[i].value);
+            vm.optStr = vm.optStr.concat("\n");
+        }
 
         function update() {
+            var optArray = vm.optStr.split("\n");
+            for(var i in field.options)
+            {
+                opt = optArray[i].split(":");
+                vm.field.options[i].label = opt[0];
+                vm.field.options[i].value = opt[1];
+            }
+
             FieldService
                 .updateField(formId, field._id, vm.field)
                 .then(function (response) {
                     $uibModalInstance.close(response);
-                })
+                });
+        }
+
+        function close() {
+            $uibModalInstance.dismiss('cancel');
         }
     }
 })();
