2bb6f23967d9521d5a4ace60b214b749f91e0c69
Ning Luo <luo.n@husky.neu.edu>
2016-04-01 23:11:27 -0400

    delete idle code

--- public/assignment/server/models/form/field.model.server.js
+++ public/assignment/server/models/form/field.model.server.js
@@ -4,7 +4,6 @@
 module.exports = function (formModel) {
 
     var Form = formModel.getMongooseModel();
-    //var Field = mongoose.model('Field', FieldSchema);
 
     var api = {
         findFieldsByFormId: findFieldsByFormId,
