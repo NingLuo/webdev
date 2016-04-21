6db25ad7b2fabfa711bd0f37c96baa7e12d7bdcd
Ning Luo <luo.n@husky.neu.edu>
2016-04-21 05:22:47 -0400

    project: refactored main page

--- public/project/client/views/main/home.control.js
+++ public/project/client/views/main/home.control.js
@@ -3,12 +3,11 @@
 
     angular
         .module("FindDoctorApp")
-        .controller("MainController", MainController);
+        .controller("MainController", MainController)
+        .controller("WarningPopCtrl", WarningPopCtrl);
 
-    function MainController ($location, $rootScope, UserService) {
-
+    function MainController ($location, $rootScope, UserService, $uibModal) {
         var vm = this;
-        vm.illegal = false;
         vm.search = search;
 
         function init() {
@@ -27,9 +26,23 @@
             if(location && specialty) {
                 $location.url('/result/specialty/' + specialty + '/location/' + location +'/insurance/' + insurance + '/gender/'+gender+'/name/'+name);
             } else {
-                console.log("Both city and specilty are required");
-                vm.illegal = true;
+                $uibModal.open(
+                    {
+                        templateUrl: 'views/main/warningPop.view.html',
+                        controller: "WarningPopCtrl as model"
+                    }
+                );
             }
+        }
+    }
+
+
+    function WarningPopCtrl($uibModalInstance) {
+        var vm = this;
+        vm.close = close;
+
+        function close() {
+            $uibModalInstance.dismiss();
         }
     }
 })();
